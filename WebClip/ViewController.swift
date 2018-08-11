3c24010814602b492ab56a3f75aa848a6bfd4a87
Toru Asai <acaibowl.dev@gmail.com>
2018-08-11 23:53:44 +0900

    add initial project

---
+++ WebClip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebClip
+//
+//  Created by Toru Asai on 2018/08/11.
+//  Copyright © 2018年 Toru Asai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
